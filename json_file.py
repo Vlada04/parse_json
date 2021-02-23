d9cb12036898696e97a1316b4004daab1f3a2ef8
Vlada04 <v.omelchenko@ucu.edu.ua>
2021-02-23 15:16:13 +0200

    Add file for test

--- json_file.py
+++ json_file.py
@@ -10,28 +10,45 @@ def read_file(file_name):
         data = json.load(json_file)
     return data
 
+
 def parsing_json_object(file_name):
+    '''
+    Parse json object using the entered key
+    '''
     for key in read_file(file_name):
         print(key)
-    first_key = input("Choose key:")
+    choose_key = input("Choose key:")
 
-    if first_key in read_file(file_name):
-        if type(read_file(file_name)[first_key]) != list and type(read_file(file_name)[first_key]) != dict:
-            return read_file(file_name).get(first_key)
+    if choose_key in read_file(file_name):
+        if type(read_file(file_name)[choose_key]) != list and\
+            type(read_file(file_name)[choose_key]) != dict:
+            return read_file(file_name).get(choose_key)
+    
+    res_lst = []
+    if choose_key in read_file(file_name):
+        if type(read_file(file_name)[choose_key]) == dict:
+            while len(res_lst) < 1:
+                choose_next_key = choose_key
+                A = read_file(file_name)[choose_next_key]
+                print([res for res in A])
+                choose_next_key = input("Choose next key:")
 
-        if type(read_file(file_name)[first_key]) == list:
-            print("It's a list:")
-            for sets in read_file(file_name)[first_key]:
-                print(sets)
-                print(' ')
-            choose_from_list = input("Choose element number(must be integer): ")
-
-            print([key for key in read_file(file_name)[first_key][int(choose_from_list)-1]])
-            second_choise = input('Choose key: ')
-            return read_file(file_name)[first_key][int(choose_from_list)-1].get(second_choise)
-
-
-    else:
-        return "Key not in file"
-
-print(parsing_json_object("kved.json"))
+                if type(A[choose_next_key]) == list:
+                    print("It's a list")
+                    for i in A[choose_next_key]:
+                        print(i)
+                    get_lst_elem = input('Choose list element index (must be integer): ')
+                    if type(A[choose_next_key][int(get_lst_elem)-1]) == dict:
+                        print([key for key in A[choose_next_key][int(get_lst_elem)-1]])
+                        choose_from_list = input('Choose key: ')
+                        res_lst.append(A[choose_next_key][int(get_lst_elem)-1][choose_from_list])
+                    if type(A[choose_next_key][int(get_lst_elem)-1]) == list:
+                        print("It's a list")
+                        res_lst.append([elem for elem in A[choose_next_key][int(get_lst_elem)-1]])
+                    else:
+                        res_lst.append(A[choose_next_key][int(get_lst_elem)-1])
+                        
+                if type(A[choose_next_key]) != list and type(A[choose_next_key]) != dict:
+                    res_lst.append(A[choose_next_key])
+            return res_lst
+    return "Key not in file"
